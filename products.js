a69399ab14957e70b355a2d69c89dacc93d7b574
nech <nech@gmail.com>
2022-01-02 23:07:44 -0300

    commit

---
+++ products.js
@@ -0,0 +1,13 @@
+let items = [
+    {id: 1, name: 'Aorus NVME SSD 1TB', price: 180, stock: true, tag: 'ssd', inCart: 0, img: './assets/ssd.png'},
+    {id: 2, name: 'Thermaltake Toughram 32GB', price: 120, stock: true, tag: 'ram', inCart: 0, img: './assets/ram.png'},
+    {id: 3, name: 'Ryzen 5600X', price: 230, stock: true, tag: 'cpu', inCart: 0, img: './assets/cpu.png'},   
+    {id: 4, name: 'Nvidia Geforce RTX 3080', price: 480, stock: true, tag: 'gpu', inCart: 0, img: './assets/gpu.png'},
+    {id: 5, name: 'NZXT KRAKEN X63', price: 155, stock: true, tag: 'cooler', inCart: 0, img: './assets/cooler.png'},
+    {id: 6, name: 'NZXT H510 Elite', price: 150, stock: true, tag: 'case', inCart: 0, img: './assets/case.png'},
+    {id: 7, name: 'Gigabyte B550 MoBo', price: 240, stock: true, tag: 'mobo', inCart: 0, img: './assets/mobo.png'},
+    {id: 8, name: 'Thermaltake Keyboard', price: 80, stock: true, tag: 'keyboard', inCart: 0, img: './assets/keyboard.png'},
+    {id: 9, name: 'Gaming RGB Desk', price: 260, stock: true, tag: 'desk', inCart: 0, img: './assets/desk.png'}
+]
+
+export default items
---
+++ script.js
@@ -0,0 +1,266 @@
+//INICIO JS SLIDER
+const slider = document.querySelector(".slider");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
+const numberOfSlides = slides.length;
+var slideNumber = 0;
+
+// slider botón siguiente
+nextBtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove("active");
+  });
+
+  slideNumber++;
+
+  if(slideNumber > (numberOfSlides - 1)){
+    slideNumber = 0;
+  }
+
+  slides[slideNumber].classList.add("active");
+  slideIcons[slideNumber].classList.add("active");
+});
+
+// slider botón anterior
+prevBtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove("active");
+  });
+
+  slideNumber--;
+
+  if(slideNumber < 0){
+    slideNumber = numberOfSlides - 1;
+  }
+
+  slides[slideNumber].classList.add("active");
+  slideIcons[slideNumber].classList.add("active");
+});
+
+// slider autoplay
+var playSlider;
+
+var repeater = () => {
+  playSlider = setInterval(function(){
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+      slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+
+    if(slideNumber > (numberOfSlides - 1)){
+      slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+  }, 4000);
+}
+repeater();
+
+//parar el slider autoplay al mouseover
+slider.addEventListener("mouseover", () => {
+  clearInterval(playSlider);
+});
+
+//iniciar el slider autoplaydenuevo al mouseout
+slider.addEventListener("mouseout", () => {
+  repeater();
+});
+
+function resetearDiv(elementID)
+{
+    document.getElementById(elementID).innerHTML = document.getElementById(elementID).innerHTML;
+}
+//FIN JS SLIDER
+
+//INICIO FUNCIONALIDAD CARTAS
+let popupViews = document.querySelectorAll('.popup-view');
+let popupBtns = document.querySelectorAll('.popup-btn');
+let closeBtns = document.querySelectorAll('.close-btn');
+
+let popup = function(popupClick) {
+    popupViews[popupClick].classList.add('active');
+}
+
+popupBtns.forEach((popupBtn, i) => {
+    popupBtn.addEventListener("click", () => {
+        popup(i);
+    });
+});
+
+closeBtns.forEach((closeBtn) => {
+    closeBtn.addEventListener("click", () => {
+        popupViews.forEach((popupView) => {
+            popupView.classList.remove('active');
+        });
+    });
+});
+
+//FIN FUNCIONALIDAD CARTAS
+
+
+
+//INICIO FUNCIONALIDAD CARRITO
+
+import items from './products.js'
+/*hacer que nombre, precio, tag, descripción e imagen sean dinámicos
+generar carrito en una table como js codepen, o más elaborado  
+que el carrito se guarde en el local storage*/
+
+let products = items
+
+/*localizo todos los botones de añadir al carrito 
+y los ingreso en una variable 'addToCart'*/
+let addToCart = document.querySelectorAll('.add-cart-btn');
+
+/*loopeo a todos esos botones y genero event listeners 
+que cada vez que escuchen un click inicialicen la funcion cartNumbers() y totalCost()*/
+for (let i = 0; i < addToCart.length; i++) {
+  addToCart[i].addEventListener('click', () => {
+    cartNumbers(products[i]);
+    totalCost(products[i]);
+    displayCart();
+  })
+}
+
+/*chequea si existen valores previos en el local storage
+asignados a la variable 'cartNumbers' y si existen los asigna
+a la variable 'productNumbers' y por ende al carrito.
+*/
+function onLoadCartNumbers () {
+  let productNumbers = localStorage.getItem('cartNumbers')
+  if (productNumbers) {
+    document.querySelector('.cart span').textContent = productNumbers;
+  }
+}
+
+/*ingresa al localstorage y chequea el valor de la variable 'cartNumbers', 
+asigna ese valor a la variable 'productNumbers' parseandolo a un número 
+(ya que devuelve un string)
+Si hay un valor asignado a 'productNumbers' entonces le suma un 1, 
+y si no lo hay, entonces le asigna el valor 1.
+tanto en carrito como en localstorage de carrito.
+*/
+function cartNumbers(products) {
+  let productNumbers = localStorage.getItem('cartNumbers');
+  productNumbers = parseInt(productNumbers)
+  if (productNumbers) {
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.querySelector('.cart span').textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem('cartNumbers', 1)
+    document.querySelector('.cart span').textContent = 1
+  }
+  setItems(products)
+}
+
+/* Asigna a la variable 'cartItems' el valor de la variable del localStorage 'productsInCart'
+y luego lo parsea (transforma en notación json).*/
+function setItems(products) {
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+
+  //Si cartItems tiene un valor previo, accedes al valor inCart del producto y le sumas 1
+  //si cartItems no tiene un valor previo, inicializas el valor de inCart a 1 
+  //y asignas a cartItems el valor del tag del producto clickeado
+  if (cartItems != null) {
+//chequea si cuando clickeo un producto devuelve undefined
+//asigno a cartItems lo que hubiera previamente y el nuevo producto
+    if(cartItems[products.tag] == undefined ){
+      cartItems = {
+        ...cartItems, [products.tag]:products
+      }
+    }
+    cartItems[products.tag].inCart += 1
+  } else {
+    products.inCart = 1;
+    cartItems = {
+      [products.tag]: products
+    }
+  }
+
+  //asigna a la variable 'productsInCart' el valor formateado en cadena de strings de cartItems
+  localStorage.setItem('productsInCart', JSON.stringify(cartItems))
+
+}
+
+//crea la variable 'cartcost' y le asigna el valor del localStorage 'totalCost'
+// luego chequea, si el 'cartCost' es diferente a null
+// parsea el valor previo y asigna a 'totalcost' el valor de cartcost + el precio del producto
+//si 'cartCost' es null, le asigna a 'totalCost' el precio del producto
+function totalCost(products) {
+  let cartCost = localStorage.getItem('totalCost'); 
+
+  if(cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem('totalCost', cartCost + products.price)
+  } else {
+    localStorage.setItem('totalCost', products.price);  
+  }
+}
+
+function displayCart() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let productsContainer = document.querySelector(".products");
+  let cartCost = localStorage.getItem('totalCost'); 
+  
+  if (cartItems && productsContainer) {
+    //console.log(cartItems)
+    productsContainer.innerHTML = '';
+    Object.values(cartItems).map(item => {
+      productsContainer.innerHTML += `
+      <div class="item">
+        <img src="./assets/close.png" class="remove-item">
+        <img src="./assets/${item.tag}.png" class="banner">
+        <span>${item.name}</span>
+      </div>
+      <div class="price">$ ${item.price}</div>
+      
+      <div class="quantity">
+      <img src="./assets/left.png" class="icons">
+      ${item.inCart}
+      <img src="./assets/right.png" class="icons">
+      </div>
+      <div class="total">$ ${item.inCart * item.price}</div>
+      `
+    });
+    
+    productsContainer.innerHTML += `
+    <div class="basketTotal">
+    <h3>TOTAL</h3>
+    <h3>$ ${cartCost}</h3>
+    </div>
+    `
+  }
+}
+
+function deleteItems() {
+  //al cliquear la imagen 'deleteIcons' se borre el elemento de esa imagen
+}
+
+
+onLoadCartNumbers();
+displayCart();
+//FIN FUNCIONALIDAD CARRITO
+
+//cosas a hacer:
+//funcion para borrar elementos del localStorage-Carrito
+//función para aumentar y disminuir la cantidad de elementos del carrito desde flechitas
+//arreglar en el index.html el hamburger menu no vuelve
+//arreglar en el cart.html el hamburger menu no funca
+//hacer que el cart sea un popup
+//hacer que al comprar un producto no me suba al principio de la web
+//estilizar carrito
